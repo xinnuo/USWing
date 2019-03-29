91a5bea4176306cda5295c6ecf8a83f522ecbbe4
xinnuo <416143467@qq.com>
2019-03-29 20:01:31 +0800

    03-29/20:01

--- app/src/main/kotlin/com/meida/uswing/WebActivity.kt
+++ app/src/main/kotlin/com/meida/uswing/WebActivity.kt
@@ -4,22 +4,19 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebChromeClient
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import com.lzg.extend.StringDialogCallback
 import com.lzy.okgo.OkGo
 import com.lzy.okgo.model.Response
-import com.meida.base.BaseActivity
-import com.meida.base.cancelLoadingDialog
-import com.meida.base.getString
-import com.meida.base.showLoadingDialog
+import com.meida.base.*
 import com.meida.share.BaseHttp
-import com.meida.utils.isWeb
+import com.meida.view.webViewX5
+import com.tencent.smtt.sdk.WebChromeClient
+import com.tencent.smtt.sdk.WebSettings
+import com.tencent.smtt.sdk.WebView
+import com.tencent.smtt.sdk.WebViewClient
 import org.jetbrains.anko.backgroundColorResource
 import org.jetbrains.anko.browse
-import org.jetbrains.anko.webView
+import org.jetbrains.anko.frameLayout
 import org.json.JSONObject
 
 class WebActivity : BaseActivity() {
@@ -29,65 +26,71 @@ class WebActivity : BaseActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        webView = webView {
-            overScrollMode = View.OVER_SCROLL_NEVER
-            backgroundColorResource = R.color.transparent
+        frameLayout {
+            webView = webViewX5 {
+                overScrollMode = View.OVER_SCROLL_NEVER
+                backgroundColorResource = R.color.transparent
 
-            //支持javascript
-            settings.javaScriptEnabled = true
-            //设置可以支持缩放
-            settings.setSupportZoom(true)
-            //自适应屏幕
-            settings.loadWithOverviewMode = true
-            settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
-            isHorizontalScrollBarEnabled = false
+                //支持javascript
+                @Suppress("DEPRECATION")
+                settings.javaScriptEnabled = true
+                //设置可以支持缩放
+                settings.setSupportZoom(true)
+                //自适应屏幕
+                settings.loadWithOverviewMode = true
+                settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+                isHorizontalScrollBarEnabled = false
 
-            //设置出现缩放工具
-            settings.builtInZoomControls = true
-            settings.displayZoomControls = false
+                //设置出现缩放工具
+                settings.builtInZoomControls = true
+                settings.displayZoomControls = false
 
-            //设置是否使用缓存
-            settings.setAppCacheEnabled(true)
-            settings.domStorageEnabled = true
+                //设置是否使用缓存
+                settings.setAppCacheEnabled(true)
+                settings.domStorageEnabled = true
 
-            webChromeClient = WebChromeClient()
-            webViewClient = object : WebViewClient() {
+                webChromeClient = WebChromeClient()
+                webViewClient = object : WebViewClient() {
 
-                /* 这个事件，将在用户点击链接时触发。
-                 * 通过判断url，可确定如何操作，
-                 * 如果返回true，表示我们已经处理了这个request，
-                 * 如果返回false，表示没有处理，那么浏览器将会根据url获取网页
-                 */
-                @Suppress("DEPRECATION", "OverridingDeprecatedMember")
-                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-
-
-                    if (!url.isWeb()) return true
-
-                    if (url.isNotEmpty() && url.endsWith("apk")) browse(url)
-                    else {
-                        view.loadUrl(url)
-                        return true
+                    /* 这个事件，将在用户点击链接时触发。
+                     * 通过判断url，可确定如何操作，
+                     * 如果返回true，表示我们已经处理了这个request，
+                     * 如果返回false，表示没有处理，那么浏览器将会根据url获取网页
+                     */
+                    @Suppress("DEPRECATION", "OverridingDeprecatedMember")
+                    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                        return when {
+                            "tel:" in url -> makeCall(url.replace("tel:", ""))
+                            !url.startsWith("https://")
+                                    && !url.startsWith("http://") -> {
+                                if (url.startsWith("weixin://wap/pay?")) browse(url)
+                                true
+                            }
+                            url.endsWith(".apk") -> browse(url)
+                            else -> {
+                                view.loadUrl(url)
+                                true
+                            }
+                        }
                     }
-                    return super.shouldOverrideUrlLoading(view, url)
-                }
 
-                /*
-                 * 在开始加载网页时会回调
-                 */
-                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-                    super.onPageStarted(view, url, favicon)
-                    showLoadingDialog()
-                }
+                    /*
+                     * 在开始加载网页时会回调
+                     */
+                    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                        super.onPageStarted(view, url, favicon)
+                        showLoadingDialog()
+                    }
 
-                /*
-                 * 在结束加载网页时会回调
-                 */
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
-                    cancelLoadingDialog()
-                }
+                    /*
+                     * 在结束加载网页时会回调
+                     */
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+                        cancelLoadingDialog()
+                    }
 
+                }
             }
         }
 
@@ -251,6 +254,7 @@ class WebActivity : BaseActivity() {
                 tvTitle.text = intent.getStringExtra("hint")
                 val urlLoad = intent.getStringExtra("url")
                 webView.loadUrl(if (urlLoad.startsWith("http")) urlLoad else BaseHttp.baseImg + urlLoad)
+                // webView.loadUrl("http://nativeapp.globalwinson.com:8092/slider/quanpain.mp4")
             }
         }
     }
