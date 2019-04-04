020619e686386ca1650cf3351e607beebfc56f5a
xinnuo <86984250@qq.com>
2019-04-04 16:48:42 +0800

    04-04/16:48

--- app/src/main/kotlin/com/meida/utils/DecimalNumberFilter.kt
+++ app/src/main/kotlin/com/meida/utils/DecimalNumberFilter.kt
@@ -2,7 +2,6 @@ package com.meida.utils
 
 import android.text.InputFilter
 import android.text.Spanned
-import android.text.TextUtils
 
 /**
  * edittext保留两位小数，控制最大值
@@ -12,26 +11,33 @@ class DecimalNumberFilter : InputFilter {
     private val decimalNumber = 2 //小数点后保留位数
 
     //source:即将输入的内容 dest：原来输入的内容
-    override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dstart: Int, dend: Int): CharSequence {
+    override fun filter(
+        source: CharSequence,
+        start: Int,
+        end: Int,
+        dest: Spanned,
+        dstart: Int,
+        dend: Int
+    ): CharSequence {
         val sourceContent = source.toString()
         val lastInputContent = dest.toString()
 
-        //验证删除等按键
-        if (TextUtils.isEmpty(sourceContent)) return ""
-
-        //以小数点"."开头，默认为设置为“0.”开头
-        if (sourceContent == "." && lastInputContent.isEmpty()) return "0."
-
-        //输入“0”，默认设置为以"0."开头
-        if (sourceContent == "0" && lastInputContent.isEmpty()) return "0."
-
-        //小数点后保留两位
-        if (lastInputContent.contains(".")) {
-            val index = lastInputContent.indexOf(".")
-            if (dend - index >= decimalNumber + 1) return ""
+        return when {
+            //验证删除等按键
+            sourceContent.isEmpty() -> ""
+            //以"0"开头，只能输入"."
+            lastInputContent == "0" && sourceContent != "." -> ""
+            //以小数点"."开头，默认为设置为"0."开头
+            sourceContent == "." && lastInputContent.isEmpty() -> "0."
+            //输入"0"，默认设置为以"0."开头
+            sourceContent == "0" && lastInputContent.isEmpty() -> "0."
+            //小数点后保留两位
+            lastInputContent.contains(".") -> {
+                val index = lastInputContent.indexOf(".")
+                if (dend - index >= decimalNumber + 1) "" else source
+            }
+            else -> source
         }
-
-        return source
     }
 
 }
