14e3dc1d5b2f9a04ad23c11a046e99118da91023
xinnuo <86984250@qq.com>
2019-04-24 15:46:21 +0800

    04-24/15:46

--- app/src/main/kotlin/com/meida/view/DropPopupWindow.kt
+++ app/src/main/kotlin/com/meida/view/DropPopupWindow.kt
@@ -28,21 +28,15 @@
 package com.meida.view
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.graphics.Color
 import android.graphics.Rect
-import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Handler
-import android.support.annotation.LayoutRes
-import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.PopupWindow
 import com.meida.uswing.R
-import com.meida.utils.getScreenWidth
 import com.meida.utils.startRotateAnimator
 
 /**
@@ -55,50 +49,41 @@ import com.meida.utils.startRotateAnimator
  * 在有默认参数值的方法中使用@JvmOverloads注解，则Kotlin就会暴露多个重载方法。
  * 该注解也可用在构造方法和静态方法
  */
-abstract class DropPopWindow @JvmOverloads constructor(
-    context: Context,
-    @LayoutRes resource: Int,
+abstract class DropPopupWindow @JvmOverloads constructor(
+    view: View,
     private val indicator: View? = null
-) : PopupWindow() {
+) : PopupWindow(
+    view,
+    WindowManager.LayoutParams.MATCH_PARENT,
+    WindowManager.LayoutParams.WRAP_CONTENT,
+    true
+) {
 
-    private val window: View = LayoutInflater.from(context).inflate(resource, null)
     private lateinit var mContainer: View
-    private val animationIn: Animation
-    private val animationOut: Animation
+    private val animationIn: Animation = AnimationUtils.loadAnimation(contentView.context, R.anim.pop_anim_show)
+    private val animationOut: Animation = AnimationUtils.loadAnimation(contentView.context, R.anim.pop_anim_dismiss)
     private var isDismiss = false
 
     init {
-        contentView = window
-        width = context.getScreenWidth()
-        height = WindowManager.LayoutParams.WRAP_CONTENT
-        animationStyle = R.style.WindowStyle
-        isFocusable = true
-        isOutsideTouchable = true
-        update()
-        setBackgroundDrawable(ColorDrawable(Color.argb(123, 0, 0, 0)))
-
-        animationIn = AnimationUtils.loadAnimation(context, R.anim.pop_anim_show)
-        animationOut = AnimationUtils.loadAnimation(context, R.anim.pop_anim_dismiss)
         initView()
-        afterInitView(window)
+        afterInitView(view)
     }
 
     private fun initView() {
-        mContainer = window.findViewById(R.id.pop_container)
+        mContainer = contentView.findViewById(R.id.pop_container)
 
         mContainer.setOnClickListener { }
-        window.findViewById<View>(R.id.pop_root).setOnClickListener { dismiss() }
+        contentView.findViewById<View>(R.id.pop_root).setOnClickListener { dismiss() }
     }
 
     abstract fun afterInitView(view: View)
 
     override fun showAsDropDown(anchor: View) {
-        if (Build.VERSION.SDK_INT >= 24) {
+        if (Build.VERSION.SDK_INT == 24) {
             val rect = Rect()
             anchor.getGlobalVisibleRect(rect)
             height = anchor.resources.displayMetrics.heightPixels - rect.bottom
         }
-
         super.showAsDropDown(anchor)
         isDismiss = false
         mContainer.startAnimation(animationIn)
@@ -118,11 +103,11 @@ abstract class DropPopWindow @JvmOverloads constructor(
 
                 @SuppressLint("ObsoleteSdkInt")
                 override fun onAnimationEnd(animation: Animation) {
-                    this@DropPopWindow.isDismiss = false
+                    this@DropPopupWindow.isDismiss = false
 
                     if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN)
-                        this@DropPopWindow.dismiss4Pop()
-                    else super@DropPopWindow.dismiss()
+                        this@DropPopupWindow.dismiss4Pop()
+                    else super@DropPopupWindow.dismiss()
                 }
 
                 override fun onAnimationRepeat(animation: Animation) {}
@@ -131,7 +116,7 @@ abstract class DropPopWindow @JvmOverloads constructor(
     }
 
     private fun dismiss4Pop() {
-        Handler().post { super@DropPopWindow.dismiss() }
+        Handler().post { super@DropPopupWindow.dismiss() }
     }
 
 }
