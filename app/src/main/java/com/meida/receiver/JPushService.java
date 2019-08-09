5fe50a1bf92e8896b3dcaeaf85ec4829ce5e8d26
xinnuo <416143467@qq.com>
2019-08-09 18:07:41 +0800

    08-09/18:07

---
+++ app/src/main/java/com/meida/receiver/JPushService.java
@@ -0,0 +1,12 @@
+package com.meida.receiver;
+
+import cn.jpush.android.service.JCommonService;
+
+/**
+ * 项目名称：USWing
+ * 创建人：小卷毛
+ * 创建时间：2019-08-09 18:05
+ */
+public class JPushService extends JCommonService {
+
+}
